cb0d9422ba850e45affb215e96bc0f1413eeeff5
L. E. Segovia <amy@amyspark.me>
2021-09-08 23:52:21 +0000

    Update handling and dependencies

--- package.rb
+++ package.rb
@@ -2,15 +2,27 @@
 
 require 'date'
 
-build_date = DateTime.now.rfc3339
+require 'optparse'
 
-vcs = `git rev-parse HEAD`.strip
+options = {}
 
-architectures = %w[armhf arm64 x86_64]
+OptionParser.new do |opts|
+  opts.banner = "Usage: package.rb [options]"
 
-architectures.each do |arch|
-  Dir.chdir __dir__ do |_wd|
-    warn "docker build --build-arg=BUILD_DATE=#{build_date} --build-arg=BUILD_REF=#{vcs} --tag=kde-appimage-base-1604-#{arch}:latest #{arch}"
-    system("docker build --build-arg=BUILD_DATE=#{build_date} --build-arg=BUILD_REF=#{vcs} --tag=kde-appimage-base-1604-#{arch}:latest #{arch}")
+  CODES = %w[x86_64 armhf arm64]
+
+  opts.on("-a", "--arch ARCH", CODES, "Architectures to build") do |arch|
+    build_date = DateTime.now.rfc3339
+    vcs = `git rev-parse HEAD`.strip
+
+    Dir.chdir __dir__ do |_wd|
+      warn "docker build --build-arg=BUILD_DATE=#{build_date} --build-arg=BUILD_REF=#{vcs} --tag=kde-appimage-base-1604-#{arch}:latest #{arch}"
+      system("docker build --build-arg=BUILD_DATE=#{build_date} --build-arg=BUILD_REF=#{vcs} --tag=kde-appimage-base-1604-#{arch}:latest #{arch}")
+    end
   end
-end
+
+  opts.on_tail("-h", "--help", "Prints this help") do
+    puts opts
+    exit
+  end
+end.parse!
